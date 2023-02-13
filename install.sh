87b6571ae08baa17f859c739b4d2e1bd3a7d1524
VISHNU PREMIL <82073583+strangedreamer4@users.noreply.github.com>
2023-02-14 03:25:47 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,29 +9,25 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install python -y
 sudo apt install python2 -y
-sudo apt install espeak -y
 sleep 2
 apt update -y 
 apt upgrade -y
 apt install python -y 
 apt install python2 -y
-apt install espeak -y
 pkg install python -y
 pkg install python2 -y
-pkg install espeak -y
 clear
 # done
+echo " Do you want to run the server or client if server press 1 or client press 2 ?
+read ans
+if [ "$ans" = "1" ];
+then
+      python3 FACR.py
+else
+      python3 FACRs.py
+fi
+
+
+
 sleep 2
-echo " WELCOME USER "
-espeak "welcome user"
-sleep 2
-clear
-echo "PLEASE ENTER YOUR NAME "
-espeak "please enter your name "
-read name
-echo "WELCOME $name TO  F A C R " 
-espeak "welcome$name"
-sleep 2
-clear
-nano FACR.py
 exit 0
