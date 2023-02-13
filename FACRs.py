fbb5ea39d594bf369402aea4e68c84ac8be633f1
VISHNU PREMIL <82073583+strangedreamer4@users.noreply.github.com>
2023-02-14 03:08:31 +0530

    Update FACRs.py

--- FACRs.py
+++ FACRs.py
@@ -1,7 +1,8 @@
 'Chat Room Connection - Client-To-Client'
-import threading
-import socket
+mport threading
 import os
+import time
+import sys
 os.system("clear")
 print( """
 	        
@@ -12,56 +13,47 @@ print( """
                              ┃┃╱╱┃╭━╮┃╰━╯┃┃┃╰╮
                              ╰╯╱╱╰╯╱╰┻━━━┻╯╰━╯
                              server v 1.2 | StRaNgEdReAmEr
-	                     for queries: https://index.tk 
+	                     for queries: oohacker008@gmail.com
 """)
-
-host = '127.0.0.1'
-port = 4444
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
-server.listen()
-clients = []
-aliases = []
-
-
-def broadcast(message):
-    for client in clients:
-        client.send(message)
-
-# Function to handle clients'connections
-
-
-def handle_client(client):
-    while True:
-        try:
-            message = client.recv(1024)
-            broadcast(message)
-        except:
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            alias = aliases[index]
-            broadcast(f'{alias} has left the chat room!'.encode('utf-8'))
-            aliases.remove(alias)
-            break
-# Main function to receive the clients connection
-
-
-def receive():
-    while True:
-        print('Server is running and listening ...')
-        client, address = server.accept()
-        print(f'connection is established with {str(address)}')
-        client.send('alias?'.encode('utf-8'))
-        alias = client.recv(1024)
-        aliases.append(alias)
-        clients.append(client)
-        print(f'The alias of this client is {alias}'.encode('utf-8'))
-        broadcast(f'{alias} has connected to the chat room'.encode('utf-8'))
-        client.send('you are now connected!'.encode('utf-8'))
-        thread = threading.Thread(target=handle_client, args=(client,))
-        thread.start()
-
+def loading_animation():
+    for i in range(100):
+        sys.stdout.write("\rLoading... %d%%" % (i+1) * 10)
+        sys.stdout.flush()
+        time.sleep(0.1)
 
 if __name__ == "__main__":
-    receive()
+    loading_animation()
+    print("\nLoading complete!")
+
+def start_client(ip, port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((ip, port))
+    print("Connected to", ip, ":", port)
+    print(client_socket.recv(1024).decode())
+    username = input("Enter your username: ")
+    client_socket.send(username.encode())
+    receive_thread = threading.Thread(target=receive_message, args=(client_socket,))
+    receive_thread.start()
+    send_message(client_socket)
+
+def receive_message(client_socket):
+    while True:
+        message = client_socket.recv(1024).decode()
+        if not message:
+            break
+        print(message)
+
+def send_message(client_socket):
+    while True:
+        message = input()
+        if message == "exit":
+            client_socket.send(message.encode())
+            break
+        client_socket.send(message.encode())
+
+if __name__ == '__main__':
+    ip = input("Enter server IP: ")
+    port = int(input("Enter server port: "))
+    start_client(ip, port)
+    
+
